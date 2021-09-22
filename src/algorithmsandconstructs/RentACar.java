8cb7001402f32ca0de056c5a480e2a2e7d8228a1
Guilherme-Stach <guilherme.stach13@gmail.com>
2021-09-22 12:27:16 +0100

    Create RentACar.java
    rentacar class

---
+++ src/algorithmsandconstructs/RentACar.java
@@ -0,0 +1,59 @@
+
+package algorithmsandconstructs;
+
+import algorithmsandconstructs.enums.Make;
+import algorithmsandconstructs.enums.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RentACar implements RentACarInterface {
+    
+    List<CarInterface> cars;
+    String name;
+    
+    public RentACar(){
+        cars = new ArrayList<>(); //initialize empty arraylist to store list of cars
+        name = "CCT Car Rental";
+    }
+    @Override
+    public List<CarInterface> getCars() {
+
+        return cars;
+    }
+
+    @Override
+    public void setCars(List<CarInterface> cars) {
+        this.cars = cars;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean checkAvailability(Month month, int day, Make make, int lengthOfRent) {
+        if ()
+    }
+
+    @Override
+    public int getCarAvailable(Month month, int day, Make make, int lengthOfRent) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean bookCar(Month month, int day, Make make, int lengthOfRent) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getNumberOfCars() {
+        return cars.size();
+    }
+    
+}
